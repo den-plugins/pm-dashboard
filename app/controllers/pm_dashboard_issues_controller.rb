ec12fb91a3a72f6c8e6a9e9f60c02e55790e7b09
liit <apadilla@g2ix.net>
2011-07-28 17:43:53 +0800

    added index page for issues tab in pm dashboard

---
+++ app/controllers/pm_dashboard_issues_controller.rb
@@ -0,0 +1,17 @@
+class PmDashboardIssuesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def add
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
---
+++ app/helpers/pm_dashboard_issues_helper.rb
@@ -0,0 +1,2 @@
+module PmDashboardIssuesHelper
+end
