7062207004480e1e6873ae59b49e4365c153c4ec
lvillasica <lvillasica@lvillasica.(none)>
2011-08-09 18:56:25 +0800

    Project Scope functionality.

--- app/controllers/pm_dashboards_controller.rb
+++ app/controllers/pm_dashboards_controller.rb
@@ -1,6 +1,7 @@
 class PmDashboardsController < ApplicationController
   helper :assumptions
   helper :project_info
+  helper :custom_fields
   
   def index
     @project = Project.find(params[:project_id])
@@ -8,6 +9,10 @@ class PmDashboardsController < ApplicationController
     @issues = @project.pm_dashboard_issues.all
     @risks = @project.risks.find(:all, :order => 'ref_number DESC')
     @members = @project.members.all(:order => "role_id")
+    @project_scope = CustomField.find_by_name("Project Scope")
+    if !@project_scope.nil?
+      @project_scope_value = @project.custom_values.find(:all, :conditions => "custom_field_id = #{@project_scope.id}")
+    end
     @stakeholders = []
     
     rescue ActiveRecord::RecordNotFound
