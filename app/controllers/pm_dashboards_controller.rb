e7fbb8a40f6227862b9f861e8ae0b720f6664875
Stefany Serino <stefanyserino@gmail.com>
2011-08-09 20:48:22 +0800

    Risks Tab updated, Assumptions Tab updated

--- app/controllers/pm_dashboards_controller.rb
+++ app/controllers/pm_dashboards_controller.rb
@@ -3,13 +3,12 @@ class PmDashboardsController < ApplicationController
   helper :risks
   helper :project_info
   helper :pm_dashboard_issues
-  helper :project_info
   
   def index
     @project = Project.find(params[:project_id])
     @assumptions = @project.assumptions.find(:all, :order => 'ref_number DESC')
     @issues = @project.pm_dashboard_issues.all
-    @risks = @project.risks.find(:all, :order => 'ref_number DESC')
+    @risks = params[:risk] ? @project.risks.find(params[:risk]).to_a : @project.risks.find(:all, :order => 'ref_number DESC')
     @members = @project.members.all(:order => "role_id")
     @project_scope = CustomField.find_by_name("Project Scope")
     if !@project_scope.nil?
--- app/models/assumption.rb
+++ app/models/assumption.rb
@@ -9,13 +9,13 @@ class Assumption < ActiveRecord::Base
   belongs_to :project
   has_one :user
   has_and_belongs_to_many :risks
-  belongs_to :pmdashboardissue
   
   validates_presence_of :description, :project, :owner, :date_due
   validates_inclusion_of :status, :in => STATUS.keys
   validates_numericality_of :days_overdue, :allow_nil => true
   validate :date_due_not_past
 
+  before_save :set_status
   before_save :set_ref_number
   before_save :set_date_closed
   
@@ -29,6 +29,10 @@ class Assumption < ActiveRecord::Base
   
   def date_due_not_past
     errors.add(:date_due, 'is in the past') if date_due and date_due.past?
+  end
+  
+  def set_status
+    self.status = STATUS_CLOSED unless risk_ids.empty?
   end
 
 end
