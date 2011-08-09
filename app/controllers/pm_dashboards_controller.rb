f4079258b9f4266cc10897d794423946f4bf957a
lvillasica <lvillasica@lvillasica.(none)>
2011-08-09 11:34:43 +0800

    Added project info view.

--- app/controllers/pm_dashboards_controller.rb
+++ app/controllers/pm_dashboards_controller.rb
@@ -1,11 +1,14 @@
 class PmDashboardsController < ApplicationController
   helper :assumptions
+  helper :project_info
   
   def index
     @project = Project.find(params[:project_id])
     @assumptions = @project.assumptions.find(:all, :order => 'ref_number DESC')
     @issues = @project.pm_dashboard_issues.all
     @risks = @project.risks.find(:all, :order => 'ref_number DESC')
+    @members = @project.members.all(:order => "role_id")
+    @stakeholders = []
     
     rescue ActiveRecord::RecordNotFound
       render_404
