7af7df2eea5acede628ecc4edc4801a565392915
lvillasica <lvillasica@lvillasica.(none)>
2011-08-09 20:26:28 +0800

    recovered lost updates due to conflict.

--- app/controllers/pm_dashboards_controller.rb
+++ app/controllers/pm_dashboards_controller.rb
@@ -1,12 +1,19 @@
 class PmDashboardsController < ApplicationController
   helper :assumptions
   helper :pm_dashboard_issues
+  helper :project_info
   
   def index
     @project = Project.find(params[:project_id])
     @assumptions = @project.assumptions.find(:all, :order => 'ref_number DESC')
     @issues = @project.pm_dashboard_issues.all
     @risks = @project.risks.find(:all, :order => 'ref_number DESC')
+    @members = @project.members.all(:order => "role_id")
+    @project_scope = CustomField.find_by_name("Project Scope")
+    if !@project_scope.nil?
+      @project_scope_value = @project.custom_values.find(:all, :conditions => "custom_field_id = #{@project_scope.id}")
+    end
+    @stakeholders = []
     
     rescue ActiveRecord::RecordNotFound
       render_404
