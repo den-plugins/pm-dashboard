b879b07a79cf49f4171ccde73bd89933fe92c6ee
Stefany Serino <stefanyserino@gmail.com>
2011-08-11 17:49:24 +0800

    updated related_attribute select columns

--- app/controllers/pm_dashboards_controller.rb
+++ app/controllers/pm_dashboards_controller.rb
@@ -3,12 +3,13 @@ class PmDashboardsController < ApplicationController
   helper :risks
   helper :project_info
   helper :pm_dashboard_issues
-  
+
+  before_filter :get_id, :only => [:index]
+    
   def index
-    @project = Project.find(params[:project_id])
-    @assumptions = @project.assumptions.find(:all, :order => 'ref_number DESC')
-    @issues = @project.pm_dashboard_issues.all
-    @risks = params[:risk] ? @project.risks.find(params[:risk]).to_a : @project.risks.find(:all, :order => 'ref_number DESC')
+    @assumptions ||= @project.assumptions.find(:all, :order => 'ref_number DESC')
+    @issues ||= @project.pm_dashboard_issues.all
+    @risks ||= @project.risks.find(:all, :order => 'ref_number DESC')
     @members = @project.members.all(:order => "role_id")
     @project_scope = CustomField.find_by_name("Project Scope")
     @user_custom_fields = CustomField.find(:all, :conditions => "type = 'UserCustomField'")
@@ -20,4 +21,20 @@ class PmDashboardsController < ApplicationController
     rescue ActiveRecord::RecordNotFound
       render_404
   end
+  
+  private
+  
+  def get_id
+    @project = Project.find(params[:project_id])
+    if params[:q]
+      case params[:tab]
+        when 'assumptions'; @assumptions = @project.assumptions.find(params[:q]).to_a
+        when 'issues'; @issues = @project.pm_dashboard_issues.find(params[:q]).to_a
+        when 'risks'; @risks = @project.risks.find(params[:q]).to_a
+      end
+    end
+    rescue ActiveRecord::RecordNotFound
+      render_404
+  end
+  
 end
