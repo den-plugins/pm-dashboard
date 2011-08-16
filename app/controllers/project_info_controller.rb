708aa3734818f43921e2b67a081829df95f1b9b4
lvillasica <lvillasica@lvillasica.(none)>
2011-08-16 12:07:15 +0800

    minor updates.

--- app/controllers/project_info_controller.rb
+++ app/controllers/project_info_controller.rb
@@ -25,9 +25,11 @@ class ProjectInfoController < ApplicationController
     else 
       @project = Project.find(params[:project_id])
       render :partial => "pm_dashboards/project_info/pm_member_add", 
-              :locals => {:classification => params[:classification]} 
+                    :locals => {:classification => params[:classification]}
     end
   end
+
+#  "#{params[:classification].downcase.sub(' ', '_')}_#{@project.id}"
 
   def pm_member_remove
     if request.post?
@@ -44,7 +46,9 @@ class ProjectInfoController < ApplicationController
     @member = Member.find(params[:id]) 
     @project = @member.project
 
-    if params[:member][params[:role_or_pos].to_sym].blank?
+    role_or_pos = params[:member][params[:role_or_pos].to_sym]
+
+    if role_or_pos.blank? or role_or_pos.eql?("Others")
       form_name = ((params[:role_or_pos].split("_")[1].eql?("role"))? "role" : "position")
       render(:update) {|page| page.replace_html "#{params[:role_or_pos].split('_')[1]}_#{params[:id]}", {:partial => 'pm_dashboards/project_info/add_role_pos', :locals => {:form_name => form_name}}}
     else
