6cdeb7acc7f3e63b6117ce26a297385c2ac48e27
lvillasica <lvillasica@lvillasica.(none)>
2011-09-16 15:02:23 +0800

    fixed week validations

--- app/models/week.rb
+++ app/models/week.rb
@@ -11,9 +11,11 @@ class Week < ActiveRecord::Base
       errors.add :start_date, "Start date should be lesser than end date." if self.from > self.to
       errors.add :date, "Dates for each colums should be in same week." if self.from.cweek != self.to.cweek
       errors.add :out_of_range, "Date out of range." if self.from < project.planned_start_date or self.to > project.planned_end_date
-      errors.add :week, "Invalid week." if !valid_weeks.flatten.member?(self.from.cweek) or !valid_weeks.flatten.member?(self.to.cweek)
-      errors.add :unique_week, "Each column should have unique weeks." if Hash[*valid_weeks.flatten][self.id] != (self.from.cweek && self.to.cweek)
+#      errors.add :week, "Invalid week." if !valid_weeks.flatten.member?(self.from.cweek) or !valid_weeks.flatten.member?(self.to.cweek)
       errors.add :week_ends, "Weekends are not considered." if self.from.wday == 0 or self.to.wday == 6
+      if Hash[*valid_weeks.flatten][self.id] and Hash[*valid_weeks.flatten][self.id] != (self.from.cweek && self.to.cweek)
+        errors.add :unique_week, "Each column should have unique weeks."
+      end
     end
   end
 end
