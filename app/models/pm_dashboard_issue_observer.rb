3e5635f6aa70ea2ce01e550dd08bdc1c63f1df1d
Stefany Serino <stefanyserino@gmail.com>
2011-09-17 19:27:07 +0800

    fixed: email notifications errors

--- app/models/pm_dashboard_issue_observer.rb
+++ app/models/pm_dashboard_issue_observer.rb
@@ -7,7 +7,6 @@ class PmDashboardIssueObserver < ActiveRecord::Observer
   end
   
   def before_update(issue)
-    issue.changes.delete_if {|k| k.eql? "updated_on" or k.eql?  "days_overdue"}
     @@changed = issue.changed? ? true :  false
   end
 
