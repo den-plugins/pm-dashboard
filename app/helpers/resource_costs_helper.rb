3609e1051110107837d729dac86e85d00ec6865f
Stefany Serino <stefanyserino@gmail.com>
2011-09-19 19:28:02 +0800

    computations monthly

--- app/helpers/resource_costs_helper.rb
+++ app/helpers/resource_costs_helper.rb
@@ -12,9 +12,9 @@ module ResourceCostsHelper
     end
   end
   
-  def get_cumulative_total(per_week, total)
-    per_week.each_value do |week|
-      total += week[2].to_f
+  def get_cumulative_total(per_column, total)
+    per_column.each_value do |col|
+      total += col[2].to_f
     end
     total
   end
