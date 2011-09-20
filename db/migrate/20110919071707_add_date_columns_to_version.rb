6f03b5d282e05271b9faaf51ed6e9d6fbc4864d0
lvillasica <lvillasica@lvillasica.(none)>
2011-09-20 18:39:21 +0800

    added columns to version

--- db/migrate/20110919071707_add_date_columns_to_version.rb
+++ db/migrate/20110919071707_add_date_columns_to_version.rb
@@ -2,10 +2,14 @@ class AddDateColumnsToVersion < ActiveRecord::Migration
   def self.up
     add_column :versions, :started_date, :date
     add_column :versions, :original_date, :date
+    add_column :versions, :version_type, :integer
+    add_column :versions, :state, :integer
   end
 
   def self.down
     remove_column :versions, :original_date
-    remove_column :versions, :start_date
+    remove_column :versions, :started_date
+    remove_column :versions, :version_type
+    remove_column :versions, :state
   end
 end
