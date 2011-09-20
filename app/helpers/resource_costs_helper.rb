6c72fbdc4eed25a21a337290c1b85ce7299755f3
Stefany Serino <stefanyserino@gmail.com>
2011-09-20 18:27:18 +0800

    small UI enhancement in resource cost forecast tab

--- app/helpers/resource_costs_helper.rb
+++ app/helpers/resource_costs_helper.rb
@@ -2,7 +2,7 @@ module ResourceCostsHelper
 
   def get_weeks(start_date, end_date)
     unless start_date.nil? or end_date.nil?
-      end_date.cweek - start_date.cweek
+      start_date.weeks_ago end_date
     end
   end
 
