c3aa9baa3fa8d1ecceffe5a9525d2520000fb721
lvillasica <lvillasica@lvillasica.(none)>
2011-09-22 11:24:38 +0800

    finalized validations for week attributes in Resource cost forecast.

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -5,9 +5,10 @@ class WeeksController < ApplicationController
 
   def new
     @week = @project.weeks.build(params[:week])
+    @week.inclosed_week = params[:inclosed_week].to_i
     if not @week.save
       err_msg = []
-      @week.errors.each { |attr, message| err_msg << "#{message}<br />" }
+      @week.errors.each { |attr, message| err_msg << "#{message}" }
       flash[:error] = err_msg.join("<br />")
     end
     redirect_to :controller => 'pm_dashboards', :action => 'index', :tab => 'resource_costs', :project_id => @project
@@ -15,9 +16,10 @@ class WeeksController < ApplicationController
 
   def edit
     @week = Week.find(params[:id])
+    @week.inclosed_week = params[:inclosed_week].to_i
     if not @week.update_attributes(params[:week])
        err_msg = []
-       @week.errors.each { |attr, message| err_msg << "#{message}<br />" }
+       @week.errors.each { |attr, message| err_msg << "#{message}" }
        flash[:error] = err_msg.join("<br />")
     end
     redirect_to :controller => 'pm_dashboards', :action => 'index', :tab => 'resource_costs', :project_id => @project
