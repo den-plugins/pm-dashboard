82f21d441a6d2ebea2d65cd78ef3d741cde15a1d
lvillasica <lvillasica@lvillasica.(none)>
2011-11-11 11:48:07 +0800

    added constants for ProjectContract

--- app/models/project_contract.rb
+++ app/models/project_contract.rb
@@ -1,5 +1,7 @@
 class ProjectContract < ActiveRecord::Base
-belongs_to :project
+  belongs_to :project
 
+  TYPES =  [['Please Select',0],['SoW',1],['CO',2]]
+  STATUSES =  [['Please Select',0],['For Sign Off',1],['With Provisional Approval',2],['Signed off',3]]
 
 end
