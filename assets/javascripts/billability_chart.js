c7bf895988446666b122a89d81a721f65fcffb34
lvillasica <lvillasica@lvillasica.(none)>
2011-11-14 18:44:47 +0800

    in progress: show billability graph on dashboard tab of pm_dashboard.

--- assets/javascripts/billability_chart.js
+++ assets/javascripts/billability_chart.js
@@ -1,13 +1,15 @@
-function plot_chart(data){
-    var plot1 = jQuery.jqplot ('jchart', data, {
+function plot_chart(id, data){
+    var plot1 = jQuery.jqplot (id, data, {
       axes: {
         // options for each axis are specified in seperate option objects.
         xaxis: {
           label: "Dates",
           renderer: jQuery.jqplot.DateAxisRenderer,
           tickOptions: {
+            showMark: false,
             fontSize: '8pt'
-          }
+          },
+          tickInterval: "14 days"
         },
         yaxis: {
           label: "Billability (%)",
