b197494893949b1dfcaf1a323650d69b8bae0bef
lvillasica <lvillasica@lvillasica.(none)>
2011-11-14 13:58:30 +0800

    in progress: validate file format

--- lib/attachment_patch.rb
+++ lib/attachment_patch.rb
@@ -5,13 +5,17 @@ module Pm
     def self.included(base)
       base.extend(ClassMethods)
       base.send(:include, InstanceMethods)
+      base.class_eval do
+         unloadable # Send unloadable so it will not be unloaded in development
+         validate :validate_file_format
+       end
     end
     
     module ClassMethods
     end
     
     module InstanceMethods
-      def validate
+      def validate_file_format
         if self.container_type == 'ProjectContract'
           unless self.is_pdf?
              errors.add(:base, :file_not_pdf)
