fed03ec3671a94d7d14065a12ce9a8f508743f84
john robert arriesgado <exst_jarriesgado@yahoo.com>
2011-11-14 11:02:27 +0800

    contracts validations in progress

--- app/models/project_contract.rb
+++ app/models/project_contract.rb
@@ -4,7 +4,8 @@ class ProjectContract < ActiveRecord::Base
   TYPES =  [['Please Select',0],['SOW',1],['CO',2]]
   STATUSES =  [['Please Select',0],['For Sign Off',1],['With Provisional Approval',2],['Signed off',3]]
 
-  acts_as_attachable 
+  acts_as_attachable :view_permission => :view_files,
+                     :delete_permission => :manage_files
 
   validates_presence_of :amount, :description
   validates_inclusion_of :pc_type, :in => 1..2
