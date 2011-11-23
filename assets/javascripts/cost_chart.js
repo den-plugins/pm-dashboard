96729a25d26f8b341456fe9c37d6981e3eeeb732
john robert arriesgado <exst_jarriesgado@yahoo.com>
2011-11-23 16:11:19 +0800

    update graph

--- assets/javascripts/cost_chart.js
+++ assets/javascripts/cost_chart.js
@@ -1,30 +1,18 @@
 function plot_cost(id, data){
-var ticks = ['Baseline Amount (Budget)', 'Forecast', 'Actual'];
-
-var plot1 = jQuery.jqplot(id, [data], {
-// Only animate if we're not using excanvas (not in IE 7 or IE 8)..
-animate: !jQuery.jqplot.use_excanvas,
-seriesDefaults:{
-    renderer:jQuery.jqplot.BarRenderer,
-    rendererOptions:{ varyBarColor : true },
-    pointLabels: { show: true }
-},
-axes: {
-    xaxis: {
-        renderer: jQuery.jqplot.CategoryAxisRenderer,
-        ticks: ticks
-    }
-},
-highlighter: { show: false },
-grid: {gridLineColor: '#f2f2f2'}  });
+	var ticks = ['Baseline Amount (Budget)', 'Forecast', 'Actual'];
+	var plot1 = jQuery.jqplot(id, [data], {
+	animate: !jQuery.jqplot.use_excanvas,
+	seriesDefaults:{
+		renderer:jQuery.jqplot.BarRenderer,
+		rendererOptions:{ varyBarColor : true },
+		pointLabels: { show: true }
+	},
+	axes: {
+		xaxis: {
+		    renderer: jQuery.jqplot.CategoryAxisRenderer,
+		    ticks: ticks
+		}
+	},
+	highlighter: { show: false },
+	grid: {gridLineColor: '#f2f2f2'}  });
 }
-     
-
-
-
-
-
-
-
-
-    
