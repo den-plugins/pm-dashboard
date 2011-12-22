2e3d029e479d7b8caca3d00b0b71e11d6c8ee7cf
Stefany Serino <stefanyserino@gmail.com>
2011-12-22 15:54:54 +0800

    minor fix: milestone states dropdown

--- lib/pm_version_patch.rb
+++ lib/pm_version_patch.rb
@@ -7,8 +7,10 @@ module Pm
       base.send(:include, InstanceMethods)
       base.class_eval do
         unloadable
-        const_set(:STATES, { 0 => 'Please Select', 1 => 'Plannning', 2 => 'Active', 3 => 'Accepted'})
+        const_set(:STATES, {1 => 'Plannning', 2 => 'Active', 3 => 'Accepted'})
         const_set(:TYPES, { 0 => "Milestone", 1 => "Development Iteration", 2 => "Build Release"})
+        
+        validates_inclusion_of :state, :in => Version::STATES.keys, :allow_nil => true
       end
     end
     
