64dd20b3a6ed159c65e8c5e8ad23004a577f87c5
lvillasica <lvillasica@lvillasica.(none)>
2012-01-09 14:07:06 +0800

    fix: Typographical error found on Taskboard

--- lib/pm_version_patch.rb
+++ lib/pm_version_patch.rb
@@ -7,7 +7,7 @@ module Pm
       base.send(:include, InstanceMethods)
       base.class_eval do
         unloadable
-        const_set(:STATES, {1 => 'Plannning', 2 => 'Active', 3 => 'Accepted'})
+        const_set(:STATES, {1 => 'Planning', 2 => 'Active', 3 => 'Accepted'})
         const_set(:TYPES, { 0 => "Milestone", 1 => "Development Iteration", 2 => "Build Release"})
         
         validates_inclusion_of :state, :in => Version::STATES.keys, :allow_nil => true
