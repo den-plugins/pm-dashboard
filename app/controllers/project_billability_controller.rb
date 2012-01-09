2cc581bedb4e76506bb31d595704c3873c968eb5
lvillasica <lvillasica@lvillasica.(none)>
2012-01-09 11:09:11 +0800

    add refresh button in billability tab

--- app/controllers/project_billability_controller.rb
+++ app/controllers/project_billability_controller.rb
@@ -22,12 +22,22 @@ class ProjectBillabilityController < ApplicationController
   end
   
   def update_project_billability
-    @billability = File.exists?("#{RAILS_ROOT}/config/billability.yml") ? YAML.load(File.open("#{RAILS_ROOT}/config/billability.yml"))["billability_#{@project.id}"] : {}
+    if @project.planned_end_date && @project.planned_start_date && params[:refresh]
+      Delayed::Job.enqueue ProjectBillabilityJob.new(@project.id)
+      temp = File.exists?("#{RAILS_ROOT}/config/billability.yml") ? YAML.load(File.open("#{RAILS_ROOT}/config/billability.yml")) : {}
+      temp.delete("billability_#{@project.id}")
+      File.open( "#{RAILS_ROOT}/config/billability.yml", 'w' ) do |out|
+        YAML.dump( temp, out )
+      end
+      @billability = nil
+    else
+      @billability = File.exists?("#{RAILS_ROOT}/config/billability.yml") ? YAML.load(File.open("#{RAILS_ROOT}/config/billability.yml"))["billability_#{@project.id}"] : {}
+    end
     render(:update) do |p|
       p.replace_html "billability_list", :partial => 'project_billability/list'
     end if params[:view]
     render(:update) do |p|
       p.replace_html :keep_loading, :partial => 'project_billability/keep_loading'
-    end if params[:processing]
+    end if params[:processing] || params[:refresh]
   end
 end
