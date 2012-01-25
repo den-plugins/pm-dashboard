602bc8004d8d8d2f84bbf227950fe4bdc9769c20
Stefany Serino <stefanyserino@gmail.com>
2012-01-25 14:07:28 +0800

    #49910: cost monitoring header data

--- app/controllers/cost_monitoring_controller.rb
+++ app/controllers/cost_monitoring_controller.rb
@@ -11,20 +11,28 @@ class CostMonitoringController < ApplicationController
     @budget_to_date, @actuals_to_date = 0, 0
     @contingency_amount = 0
     @project_budget = 0
+    @estimate_to_complete = 0
+    
+    @highlights = @project.weekly_highlights
+    highlight =  (@highlights[:current] || @highlights[:after_current] || @highlights[:posted_current] || @highlights[:posted_after_current])
+    reporting_period = highlight ? highlight.created_at.beginning_of_month : nil
 
     from, to = @project.planned_start_date, @project.planned_end_date
     if from && to
-      months = get_months_range(from, to)
-      current_range = from .. (Date.today-1.month).end_of_month
       team = @project.members.project_team.all
+      months = get_months_range(from, to)
+      forecast_range = from .. (Date.today-1.month).end_of_month
+      to_complete_range = reporting_period .. to if reporting_period
+
       @mcost = @project.monitored_cost(months, team)
       @mcost.each do |k, v|
         @bac_hours += v[:budget_hours]
         @bac_amount += v[:budget_cost]
-        if current_range.include?(k.to_date)
+        if forecast_range.include?(k.to_date)
           @budget_to_date += v[:budget_cost]
           @actuals_to_date += v[:actual_cost]
         end
+        @estimate_to_complete += v[:budget_cost] if reporting_period && to_complete_range.include?(k.to_date)
       end
       @contingency_amount = @bac_amount.to_f * (@project.contingency.to_f/100)
       @project_budget = @bac_amount + @contingency_amount
@@ -37,5 +45,4 @@ private
     rescue ActiveRecord::RecordNotFound
       render_404
   end
-
 end
