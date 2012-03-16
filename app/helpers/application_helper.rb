77d9bf1c493601b6b7808d0b002e5fec095ec9cd
Stefany Serino <stefanyserino@gmail.com>
2012-03-16 14:13:42 +0800

    fix #53365: access error Public projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def render_main_menu_for_client(project)
     if project
       member = project.members.find(:first, :conditions => ["user_id=?", User.current.id])
-      client = member if !User.current.admin? and (role=member.role) and role.name.downcase.include?('clients')
+      client = member if member and !User.current.admin? and (role=member.role) and role.name.downcase.include?('clients')
     end
     if client
       render_main_menu(project) +
