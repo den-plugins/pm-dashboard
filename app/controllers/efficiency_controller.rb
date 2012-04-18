844341fb58a0429075c7074ee680caade11da265
Gillian Dugay <gdugay@exist.com>
2012-04-18 15:57:32 +0800

    #54931. Fixed closed bug count computation; added update test code coverage

--- app/controllers/efficiency_controller.rb
+++ app/controllers/efficiency_controller.rb
@@ -1,10 +1,21 @@
 class EfficiencyController < PmController
-  before_filter :get_project, :only => [:index, :add, :update, :destroy]
+  before_filter :get_project, :only => [:index, :update_test_code_coverage]
   before_filter :authorize
   before_filter :role_check_client
 
   def index
     @versions = @project.versions.all(:order => 'effective_date IS NULL, effective_date ASC')
+  end
+
+  def update_test_code_coverage
+    if coverage_field = ProjectCustomField.find_by_name('Test Code Coverage')
+      if coverage_value = @project.custom_values.detect { |v| v.custom_field.name == 'Test Code Coverage' }
+        coverage_value.update_attribute :value, params[:coverage].to_f.to_s
+      else
+        @project.custom_values.build(:custom_field => coverage_field, :value => params[:coverage].to_f.to_s).save
+      end
+      head :ok
+    end
   end
 
 private
