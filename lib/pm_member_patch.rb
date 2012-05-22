847506e4eb92046988efaad89f48c0b784c5d8e7
Normz <johnnormancapule@gmail.com>
2012-05-22 19:21:48 +0800

    Updated computations for taskboard dist summary to include holidays

--- lib/pm_member_patch.rb
+++ lib/pm_member_patch.rb
@@ -115,10 +115,15 @@ module Pm
       def resource_day_capacity(week)
         days = 0
         allocations = resource_allocations
+        locations = [6]
+        locations << Holiday::LOCATIONS.select{|p,x| x == "#{user.location}"}.flatten[0]
+        locations << 3 if user.location.eql?('Cebu') || user.location.eql?('Manila')
+
         unless allocations.empty?
           week.each do |day|
             allocation = allocations.detect{ |a| a.start_date <= day && a.end_date >= day}
-            if allocation && (1..5) === day.wday
+            holiday = Holiday.find(:all, :conditions => ["event_date=? AND location IN (?)", day, locations])
+            if allocation && (1..5) === day.wday && holiday.empty?
               div = (allocation.resource_allocation.to_f / 100)
               days += (8 * div)
             end
