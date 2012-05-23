de02905924877c097199ca405f35f08bb07574f0
Normz <johnnormancapule@gmail.com>
2012-05-23 14:48:42 +0800

    Moved custom taskboard functions to scrum_task_board plugin

--- lib/pm_member_patch.rb
+++ lib/pm_member_patch.rb
@@ -112,26 +112,6 @@ module Pm
         rate ? [days, cost] : days
       end
       
-      def resource_day_capacity(week)
-        days = 0
-        allocations = resource_allocations
-        locations = [6]
-        locations << Holiday::LOCATIONS.select{|p,x| x == "#{user.location}"}.flatten[0]
-        locations << 3 if user.location.eql?('Cebu') || user.location.eql?('Manila')
-
-        unless allocations.empty?
-          week.each do |day|
-            allocation = allocations.detect{ |a| a.start_date <= day && a.end_date >= day}
-            holiday = Holiday.find(:all, :conditions => ["event_date=? AND location IN (?)", day, locations])
-            if allocation && (1..5) === day.wday && holiday.empty?
-              div = (allocation.resource_allocation.to_f / 100)
-              days += (8 * div)
-            end
-          end
-        end
-        days
-      end
-      
       def is_shadowed?(day)
         if a = resource_allocations.detect{ |a| a.start_date <= day && a.end_date >= day}
           a.resource_type.eql?(2)
